7cc8b7c07902c4899c4850272fd03756beb326a8
Erlinda Chambi <chambimanzanoerlinda@gmail.com>
2021-08-09 17:36:59 -0400

    Layout Crear Usuario

--- App.js
+++ App.js
@@ -7,121 +7,12 @@
  * @flow strict-local
  */
 
-import React, {Component, PureComponent} from 'react';
-import {StyleSheet, TouchableOpacity, View, Text} from 'react-native';
-import CustomButton from './components/button';
-import ActionButtons from './components/actionButtons';
-
-//PureComponent es una extension que permite renderizar o no dependiendo si el valor cambio o no
-class App extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0,
-      flag: false,
-    };
-    this.handleUp = this.handleUp.bind(this);
-    this.handleDown = this.handleDown.bind(this);
-    this.handleReset = this.handleReset.bind(this);
-    this.handlePlus10 = this.handlePlus10.bind(this);
-  }
-
-  handleUp() {
-    const {counter: ct} = this.state;
-    this.setState({counter: ct + 1});
-  }
-
-  handleDown() {
-    const {counter: ct} = this.state;
-    this.setState({counter: ct - 1});
-  }
-
-  handleReset() {
-    this.setState({counter: 0});
-  }
-
-  handlePlus10() {
-    const {counter: ct} = this.state;
-    this.setState({counter: ct + 10});
-  }
-
-  //evitar rerenders
-  /* shouldComponentUpdate(nextProps, nextState) {
-    const {counter} = this.state;
-    if (nextState.counter === counter) return false;
-    return true;
-  } */
-
-  render() {
-    const {counter} = this.state;
-    console.log('render');
-    return (
-      <View style={styles.container}>
-        <View style={styles.subcontanier}>
-          <CustomButton label="-" action={this.handleDown} />
-          <View style={styles.counterContanier}>
-            <Text style={styles.counter}>{counter}</Text>
-          </View>
-          <CustomButton label="+" action={this.handleUp} />
-        </View>
-        <View style={styles.subcontanierReset}>
-          <ActionButtons reset={this.handleReset} plus={this.handlePlus10} />
-        </View>
-      </View>
-    );
-  }
+import React, { Component } from 'react';
+import CreateUser from './app/views/createUser';
+class App extends Component {
+	render() {
+		return <CreateUser />;
+	}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#2c3e50',
-    justifyContent: 'center',
-  },
-  subcontanier: {
-    height: 50,
-    width: '100%',
-    paddingHorizontal: 10,
-    flexDirection: 'row',
-  },
-  subcontanierReset: {
-    height: 50,
-    width: '100%',
-    paddingHorizontal: 10,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 20,
-  },
-  btn: {
-    width: 50,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ecf0f1',
-  },
-  btnTxt: {
-    fontSize: 18,
-    color: '#95a5a6',
-    fontWeight: 'bold',
-  },
-  counterContanier: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  counter: {
-    fontSize: 40,
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  btnReset: {
-    height: 50,
-    width: '80%',
-    backgroundColor: '#ecf0f1',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default App;
---
+++ app/views/createUser/index.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, TextInput, TouchableOpacity, Touchable } from 'react-native';
+
+class CreateUser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Email: 'Hello',
+      Password: null,
+      Phone: null,
+    };
+  }
+
+  render() {
+    const { Email, Password, Phone } = this.state;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Email: </Text>
+        <TextInput
+          style={styles.text}
+          value={Email}
+          onChangeText={val => this.setState({ Email: val })}
+        />
+        <Text style={styles.title}>Password: </Text>
+        <TextInput
+          style={styles.text}
+          value={Password}
+          onChangeText={val => this.setState({ Password: val })}
+        />
+        <Text style={styles.title}>Phone: </Text>
+        <TextInput
+          style={styles.text}
+          value={Phone}
+          onChangeText={val => this.setState({ Phone: val })}
+        />
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => {
+            console.log({ Email, Password, Phone });
+          }}>
+          <Text style={styles.title}>Save</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#34495e',
+    paddingVertical: 20,
+    paddingHorizontal: 30,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+  text: {
+    borderWidth: 1,
+    borderColor: '#fff',
+    height: 45,
+    width: '100%',
+    paddingHorizontal: 10,
+    color: '#fff',
+  },
+  btn: {
+    borderWidth: 1,
+    borderColor: '#fff',
+    height: 45,
+    width: '100%',
+    marginTop: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default CreateUser;
