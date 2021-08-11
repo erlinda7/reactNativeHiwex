67598be470d7c23aca0e254529d2ed68d0ff90d0
Erlinda Chambi <chambimanzanoerlinda@gmail.com>
2021-08-11 16:38:23 -0400

    status de sesion

--- app/views/login/index.js
+++ app/views/login/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { View, Image, Touchable, TouchableOpacity, Text } from 'react-native';
+import { View, Image, TouchableOpacity, Text } from 'react-native';
+
+import auth from '@react-native-firebase/auth';
 
 import Loading from '../../components/loading';
 import Button from '../../components/button';
@@ -14,13 +16,28 @@ class Login extends Component {
     this.state = {
       Email: null,
       Password: null,
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    auth().onAuthStateChanged(usr => {
+      console.log({ usr });
+      if (usr) {
+        this.setState(
+          () => ({ loading: false }),
+          () => this.props.navigation.navigate('Post'),
+        );
+      } else {
+        this.setState({ loading: false });
+      }
+    });
+  }
+
   render() {
-    const { Email, Password } = this.state;
+    const { Email, Password, loading } = this.state;
     return (
-      <Loading loading={false}>
+      <Loading loading={loading}>
         <View style={styles.container}>
           <View style={styles.subcontainer}>
             <Image source={userImg} style={styles.img} />
@@ -46,11 +63,11 @@ class Login extends Component {
               title="Login"
               action={() => {
                 console.log({ Email, Password });
-                this.props.navigation.navigate('CreateUser', { ID: 1 });
-                /* auth()
+                //this.props.navigation.navigate('CreateUser', { ID: 1 });
+                auth()
                   .signInWithEmailAndPassword(Email, Password)
-                  .then(usr => this.props.navigation.navigate('CreateUser'))
-                  .catch(err => console.log({ err })); */
+                  .then(usr => this.props.navigation.navigate('Post'))
+                  .catch(err => console.log({ err }));
               }}
             />
             <TouchableOpacity
