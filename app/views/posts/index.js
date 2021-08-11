449e20e80dace781f0299b7de6af2b0be7958c16
Erlinda Chambi <chambimanzanoerlinda@gmail.com>
2021-08-11 16:53:43 -0400

    listas con metodo map

--- app/views/posts/index.js
+++ app/views/posts/index.js
@@ -1,31 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import {
-  ViewBase,
-  TextInput,
-  TouchableOpacity,
-  Text,
-  Image,
-  View,
-} from 'react-native';
+import { View, ScrollView } from 'react-native';
 
 import Button from '../../components/button';
 
 import { styles } from './styles';
 
 const Posts = () => {
-
+  const arr = [
+    { name: 'name 1' },
+    { name: 'name 2' },
+    { name: 'name 3' },
+    { name: 'name 4' },
+    { name: 'name 5' },
+    { name: 'name 6' },
+    { name: 'name 7' },
+    { name: 'name 8' },
+    { name: 'name 9' },
+    { name: 'name 10' },
+    { name: 'name 11' },
+    { name: 'name 12' },
+  ];
   return (
     <View style={styles.container}>
-      <Button
-        title="use"
-        action={() => {
-        }}
-      />
-      <Button
-        title="count"
-        action={() => {
-        }}
-      />
+      <ScrollView>
+        {arr.map(i => (
+          <Button title={i.name} action={() => console.log(i.name)} />
+        ))}
+      </ScrollView>
     </View>
   );
 };
